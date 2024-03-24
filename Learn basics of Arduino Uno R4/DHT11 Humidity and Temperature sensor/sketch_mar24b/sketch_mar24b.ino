a4788a960b2423ed57e98dec35b5f40d01aa3b2d
Adittya Kumar Chowdhury <adittyakumar671@gmail.com>
2024-03-24 23:28:43 +0600

    Added code and circuit diagram for DHT11 Temperature and Humidity sensor

---
+++ Learn basics of Arduino Uno R4/DHT11 Humidity and Temperature sensor/sketch_mar24b/sketch_mar24b.ino
@@ -0,0 +1,52 @@
+// DHT11 Temperature and Humidity Sensor interfacing with Arduino Uno
+// Code by Adittya Kumar Chowdhury
+
+#include "DHT.h"
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahrenheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print("%  Temperature: ");
+  Serial.print(t);
+  Serial.print("°C ");
+  Serial.print(f);
+  Serial.print("°F  Heat index: ");
+  Serial.print(hic);
+  Serial.print("°C ");
+  Serial.print(hif);
+  Serial.println("°F");
+}

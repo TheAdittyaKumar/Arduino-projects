1c1476532a2a8a159db12868085b35c5eebf7cd7
Adittya Kumar Chowdhury <adittyakumar671@gmail.com>
2024-04-04 00:03:25 +0600

    Made one addition, now when button is pushed you see an output in the serial monitor as well

--- Learn basics of Arduino Uno R4/Buzzer/Press button to play Active buzzer/sketch_apr3b/sketch_apr3b.ino
+++ Learn basics of Arduino Uno R4/Buzzer/Press button to play Active buzzer/sketch_apr3b/sketch_apr3b.ino
@@ -1,6 +1,6 @@
 // Code by Adittya Kumar Chowdhury
 
-// This code allows you to play the buzzer when you push the button in your 2 pin push button
+// This code allows you to play the buzzer when you push the button on your 2 pin push button and outputs the status to the serial monitor.
 
 // Define pin connections & variables
 const int buttonPin = 2; // Pin connected to the push button
@@ -9,6 +9,7 @@ const int buzzerPin = 9; // Pin connected to the buzzer
 void setup() {
   pinMode(buzzerPin, OUTPUT); // Set buzzer pin as output
   pinMode(buttonPin, INPUT_PULLUP); // Set button pin as input with internal pull-up resistor
+  Serial.begin(9600); // Initialize serial communication at 9600 bits per second
 }
 
 void loop() {
@@ -17,8 +18,17 @@ void loop() {
 
   // Check if button is pressed
   if (buttonState == LOW) { // When the button is pressed (circuit closed), it reads LOW
-    digitalWrite(buzzerPin, LOW); // Turn buzzer off
+    digitalWrite(buzzerPin, LOW); // Turn buzzer on
+    Serial.println("---------------");
+    Serial.println("Button pressed, buzzer on!");
+    Serial.println("---------------");
   } else {
-    digitalWrite(buzzerPin, HIGH); // Turn buzzer on
+    digitalWrite(buzzerPin, HIGH); // Turn buzzer off
+    Serial.println("---------------");
+    Serial.println("Button not pressed, buzzer off.");
+    Serial.println("---------------");
   }
+
+  // Delay a bit to avoid bouncing issues and to not flood the serial monitor
+  delay(200);
 }
